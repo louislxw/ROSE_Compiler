455c782f45eada315e1189554a41aa1c6cb706b6
LI XIANGWEI <louislxw@gmail.com>
2017-06-21 16:31:12 +0800

    60x60 atax kernel with unroll factor = 4

---
+++ atax_60x60_4/rose_test5.c
@@ -0,0 +1,64 @@
+#define NX 60
+#define NY 60
+int i;
+int j;
+int tmp[60];
+int y[60];
+int x[60];
+int A[60][60];
+
+int main()
+{
+  for (i = 0; i < 60; i += 1) {
+    y[i] = 0;
+  }
+  for (i = 0; i < 60; i += 1) {
+    tmp[i] = 0;
+  }
+  int _lt_var_i;
+  for (_lt_var_i = 0; _lt_var_i <= 59; _lt_var_i += 8) {
+    for (i = _lt_var_i; i <= (((59 < (_lt_var_i + 2 - 1))?59 : (_lt_var_i + 2 - 1))); i += 4) {
+      for (j = 0; j <= 59; j += 3) {
+        tmp[i] = tmp[i] + A[i][j] * x[j];
+{
+          tmp[i] = tmp[i] + A[i][j + 1] * x[j + 1];
+        }
+{
+          tmp[i] = tmp[i] + A[i][j + 2] * x[j + 2];
+        }
+{
+          tmp[i + 1] = tmp[i + 1] + A[i + 1][j] * x[j];
+{
+            tmp[i + 1] = tmp[i + 1] + A[i + 1][j + 1] * x[j + 1];
+          }
+{
+            tmp[i + 1] = tmp[i + 1] + A[i + 1][j + 2] * x[j + 2];
+          }
+        }
+{
+          tmp[i + 2] = tmp[i + 2] + A[i + 2][j] * x[j];
+{
+            tmp[i + 2] = tmp[i + 2] + A[i + 2][j + 1] * x[j + 1];
+          }
+{
+            tmp[i + 2] = tmp[i + 2] + A[i + 2][j + 2] * x[j + 2];
+          }
+        }
+{
+          tmp[i + 3] = tmp[i + 3] + A[i + 3][j] * x[j];
+{
+            tmp[i + 3] = tmp[i + 3] + A[i + 3][j + 1] * x[j + 1];
+          }
+{
+            tmp[i + 3] = tmp[i + 3] + A[i + 3][j + 2] * x[j + 2];
+          }
+        }
+      }
+    }
+  }
+  for (i = 0; i < 60; i += 1) {
+    for (j = 0; j < 60; j += 1) {
+      y[j] = y[j] + A[i][j] * tmp[i];
+    }
+  }
+}
---
+++ atax_60x60_4/run.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+############################################
+############################################
+## Author : Louis Li              	  	  ##
+##          NTU                 	  	  ##
+##				          			  	  ##
+## Notes  : Single Script File To Run 	  ##
+##	    ROSE Compiler for kernels.	  	  ##
+##					  					  ##
+############################################
+############################################
+
+
+#----------------------------- Compute tmp -----------------------------------#
+#step 1: unroll j loop in factor of u1 (2nd loop)
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopUnrolling -c test1.c -rose:loopunroll:abstract_handle "Statement<position,19>" -rose:loopunroll:factor 3
+
+cp rose_test1.c test2.c
+
+#step 2: loop interchange for i and j
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopInterchange -c test2.c -rose:loopInterchange:abstract_handle "ForStatement<numbering,3>" -rose:loopInterchange:depth 2 -rose:loopInterchange:order 1
+
+cp rose_test2.c test3.c
+
+#step 3: unroll i loop in factor of u2
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopUnrolling -c test3.c -rose:loopunroll:abstract_handle "Statement<position,19>" -rose:loopunroll:factor 4
+
+cp rose_test3.c test4.c
+
+#step 4: loop interchange for i and j
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopInterchange -c test4.c -rose:loopInterchange:abstract_handle "ForStatement<numbering,3>" -rose:loopInterchange:depth 2 -rose:loopInterchange:order 1
+
+cp rose_test4.c test5.c
+
+#step 5: loop tiling for i in factor of 15
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopTiling -c test5.c -rose:loopTiling:abstract_handle "ForStatement<numbering,3>" -rose:loopTiling:depth 1 -rose:loopTiling:tilesize 15
+
+cp rose_test5.c test6.c
+
+sed -i 's/_lt_var_i/ii/g' test6.c
+
+
+#------------------------------ Compute y -----------------------------------#
+#step 6: loop interchange for i and j
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopInterchange -c test6.c -rose:loopInterchange:abstract_handle "ForStatement<numbering,6>" -rose:loopInterchange:depth 2 -rose:loopInterchange:order 1
+
+cp rose_test6.c test7.c
+
+#step 7: unroll i loop in factor of u1
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopUnrolling -c test7.c -rose:loopunroll:abstract_handle "Statement<position,60>" -rose:loopunroll:factor 3
+
+cp rose_test7.c test8.c
+
+#step 8: loop interchange for i and j
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopInterchange -c test8.c -rose:loopInterchange:abstract_handle "ForStatement<numbering,6>" -rose:loopInterchange:depth 2 -rose:loopInterchange:order 1
+
+cp rose_test8.c test9.c
+
+#step 9: unroll j loop in factor of u2
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopUnrolling -c test9.c -rose:loopunroll:abstract_handle "Statement<position,60>" -rose:loopunroll:factor 4
+
+cp rose_test9.c test10.c
+
+#step 10: loop tiling for i in factor of 15
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopTiling -c test10.c -rose:loopTiling:abstract_handle "ForStatement<numbering,6>" -rose:loopTiling:depth 2 -rose:loopTiling:tilesize 15
+
+cp rose_test10.c test11.c
+
+sed -i 's/_lt_var_j/jj/g' test11.c
+
+#step 11: loop interchange for i and j
+/home/demo/build-rose/tests/nonsmoke/functional/roseTests/astInterfaceTests/loopInterchange -c test11.c -rose:loopInterchange:abstract_handle "ForStatement<numbering,6>" -rose:loopInterchange:depth 3 -rose:loopInterchange:order 1
+
+cp rose_test11.c test12.c
+
